9e43825ed725ed7962d5a12ca5ad7808a6ee1dac
Mowalky <dev@mowalky.com>
2021-12-18 12:21:19 -0600

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import SlotCard from "./components/SlotCard.vue";
+import Job from "./types/Job";
+
+const jobs = ref<Job[]>([
+  {
+    title: "Developer",
+    location: "NY",
+    description: "Freelance",
+    company: "Google",
+    url: "https://google.com",
+  },
+]);
+
+const addJob = (newJob: Job) => {
+  jobs.value.push(newJob);
+};
+
+const removeJob = (index: number) => {
+  jobs.value.splice(index, 1);
+};
 </script>
 
 <template>
   <SlotCard>
     <div>
       <slot name="header">test</slot>
-      <slot>22</slot>
+      <slot
+        ><ul
+          @dblclick="removeJob(i)"
+          v-for="({ title, location }, i) in jobs"
+          :key="title"
+        >
+          <li>{{ i }}. {{ title }} - {{ location }}</li>
+        </ul></slot
+      >
       <slot name="footer">footer</slot>
     </div>
   </SlotCard>
@@ -15,7 +43,22 @@ import SlotCard from "./components/SlotCard.vue";
       <slot name="header"
         ><h4 style="background-color: green">new header</h4></slot
       >
-      <slot><p>lipsum</p></slot>
+      <slot
+        ><p>lipsum</p>
+        <button
+          @click="
+            addJob({
+              title: 'new job' + Math.random(),
+              location: 'new location',
+              description: 'new description',
+              company: 'new company',
+              url: 'new url',
+            })
+          "
+        >
+          add job
+        </button></slot
+      >
       <slot name="footer"
         ><img
           src="https://www.google.com/logos/doodles/2021/seasonal-holidays-2021-6753651837109324-s.png"
---
+++ src/types/Job.ts
@@ -0,0 +1,9 @@
+interface Job {
+    title: string;
+    location: string;
+    description: string;
+    company: string;
+    url: string;
+}
+
+export default Job;
